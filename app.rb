7caf912cc98c324af128fb08112841fd75607f26
Adam Dittebrand <Adam Dittebrand>
2016-10-11 10:58:44 -0400

    got number of students, trying to get text boxes.

--- app.rb
+++ app.rb
@@ -3,7 +3,17 @@ require_relative 'random_pair.rb'
 require_relative 'formatter.rb'
 
 get '/' do
-    erb :get_names
+    erb :student_number
+end
+
+post '/number' do
+	number = params[:student_number]
+	redirect '/names?input_name=' + number
+	erb :get_names, :locals => {:student_number => number}
+end
+
+get '/names' do
+	erb :get_names
 end
 
 post '/names' do
